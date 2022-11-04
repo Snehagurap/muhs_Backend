eac2a1e5cee955cd7d6b8bc6e460e4dbc2aee3ff
Tania1511 <taniarajput.scorpio@gmail.com>
2022-11-04 09:53:18 +0530

    employeeBean edit

--- global-service/src/main/java/in/cdac/university/globalService/controller/TemplateHeaderController.java
+++ global-service/src/main/java/in/cdac/university/globalService/controller/TemplateHeaderController.java
@@ -1,9 +1,16 @@
 package in.cdac.university.globalService.controller;
 
+import in.cdac.university.globalService.bean.TemplateHeaderBean;
 import in.cdac.university.globalService.service.TemplateHeaderService;
+import in.cdac.university.globalService.util.ListPageUtility;
+import in.cdac.university.globalService.util.RequestUtility;
+import in.cdac.university.globalService.util.ResponseHandler;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+import java.util.Date;
 
 @RestController
 @RequestMapping("/global/template/header")
@@ -11,4 +18,40 @@ public class TemplateHeaderController {
 
     @Autowired
     private TemplateHeaderService templateHeaderService;
+
+    @GetMapping("listPage")
+    public ResponseEntity<?> getListPageData() throws Exception {
+        return ResponseHandler.generateOkResponse(
+                ListPageUtility.generateListPageData(templateHeaderService.listPageData())
+        );
+    }
+
+    @GetMapping("{headerId}")
+    public ResponseEntity<?> getTemplateHeaderById(@PathVariable("headerId") Long headerId) throws Exception {
+        return ResponseHandler.generateResponse(
+                templateHeaderService.getTemplateHeaderById(headerId)
+        );
+    }
+
+    @PostMapping("save")
+    public ResponseEntity<?> save(@Valid @RequestBody TemplateHeaderBean templateHeaderBean) throws Exception {
+        templateHeaderBean.setUnumIsvalid(1);
+        templateHeaderBean.setUdtEffFrom(new Date());
+        templateHeaderBean.setUdtEntryDate(new Date());
+        templateHeaderBean.setUnumUnivId(RequestUtility.getUniversityId());
+        templateHeaderBean.setUnumEntryUid(RequestUtility.getUserId());
+        return ResponseHandler.generateResponse(
+                templateHeaderService.save(templateHeaderBean)
+        );
+    }
+
+//    @PutMapping("update")
+//    public ResponseEntity<?> update(@Valid @RequestBody TemplateHeaderBean templateHeaderBean) throws Exception {
+//        templateHeaderBean.setUdtEntryDate(new Date());
+//        templateHeaderBean.setUnumUnivId(RequestUtility.getUniversityId());
+//        templateHeaderBean.setUnumEntryUid(RequestUtility.getUserId());
+//        return ResponseHandler.generateResponse(
+//                templateHeaderService.update(templateHeaderBean)
+//        );
+//    }
 }
--- global-service/src/main/java/in/cdac/university/globalService/service/TemplateHeaderService.java
+++ global-service/src/main/java/in/cdac/university/globalService/service/TemplateHeaderService.java
@@ -1,12 +1,88 @@
 package in.cdac.university.globalService.service;
 
+import in.cdac.university.globalService.bean.TemplateHeaderBean;
+import in.cdac.university.globalService.entity.GmstConfigTemplateHeaderMst;
 import in.cdac.university.globalService.repository.TemplateHeaderRepository;
+import in.cdac.university.globalService.util.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.Optional;
 
 @Service
 public class TemplateHeaderService {
 
     @Autowired
+    private Language language;
+
+    @Autowired
     private TemplateHeaderRepository templateHeaderRepository;
+
+    @Autowired
+    private RestUtility restUtility;
+    public List<TemplateHeaderBean> listPageData() throws Exception {
+        return BeanUtils.copyListProperties(
+                templateHeaderRepository.findByUnumIsvalidAndUnumUnivIdOrderByUstrTemplHeadCodeAsc(1, RequestUtility.getUniversityId()),
+                TemplateHeaderBean.class
+        );
+    }
+
+    public ServiceResponse getTemplateHeaderById(Long headerId) throws Exception {
+        Optional<GmstConfigTemplateHeaderMst> gmstConfigTemplateHeaderMstOptional = templateHeaderRepository.findByUnumTemplHeadIdAndUnumIsvalidAndUnumUnivId(
+                headerId, 1, RequestUtility.getUniversityId());
+
+        if(gmstConfigTemplateHeaderMstOptional.isEmpty()) {
+            return ServiceResponse.errorResponse(language.notFoundForId("Template Header", headerId));
+        }
+
+        TemplateHeaderBean templateHeaderBean = BeanUtils.copyProperties(gmstConfigTemplateHeaderMstOptional.get(), TemplateHeaderBean.class);
+
+        return ServiceResponse.builder()
+                .status(1)
+                .responseObject(templateHeaderBean)
+                .build();
+    }
+    
+    @Transactional
+    public ServiceResponse save(TemplateHeaderBean templateHeaderBean) {
+        //Duplicate Check
+        List<GmstConfigTemplateHeaderMst> gmstConfigTemplateHeaderMstList = templateHeaderRepository.findByUstrHeadPrintTextIgnoreCaseAndUnumIsvalidAndUnumUnivId(
+                templateHeaderBean.getUstrHeadPrintText(), 1, templateHeaderBean.getUnumUnivId()
+        );
+
+        if(!gmstConfigTemplateHeaderMstList.isEmpty()) {
+            return ServiceResponse.errorResponse(language.duplicate("Header", templateHeaderBean.getUstrHeadPrintText()));
+        }
+
+        // Generate new header id
+        GmstConfigTemplateHeaderMst gmstConfigTemplateHeaderMst = BeanUtils.copyProperties(templateHeaderBean, GmstConfigTemplateHeaderMst.class);
+        gmstConfigTemplateHeaderMst.setUnumTemplHeadId(templateHeaderRepository.getNextId());
+        templateHeaderRepository.save(gmstConfigTemplateHeaderMst);
+
+        return ServiceResponse.builder()
+                .status(1)
+                .message(language.saveSuccess("Template Header"))
+                .build();
+    }
+
+//    @Transactional
+//    public ServiceResponse update(TemplateHeaderBean templateHeaderBean) {
+//        if(templateHeaderBean.getUnumTemplHeadId() == null) {
+//            return ServiceResponse.errorResponse(language.mandatory("Header Id"));
+//        }
+//
+//        //Duplicate Check
+//        List<GmstConfigTemplateHeaderMst> gmstConfigTemplateHeaderMstList = templateHeaderRepository.findByUnumTemplHeadIdNotAndUstrHeadPrintTextAndUnumIsvalidAndUnumUnivId(
+//                templateHeaderBean.getUnumTemplHeadId(), templateHeaderBean.getUstrHeadPrintText(), 1, templateHeaderBean.getUnumUnivId()
+//        );
+//
+//        if(!gmstConfigTemplateHeaderMstList.isEmpty()) {
+//            return ServiceResponse.errorResponse(language.duplicate("Header", templateHeaderBean.getUstrHeadPrintText()));
+//        }
+//
+//        // Create Log
+//        int noOfRowsAffected = templateHeaderRepository.createLog(List.of(templateHeaderBean.getUnumTemplHeadId()));
+//    }
 }
