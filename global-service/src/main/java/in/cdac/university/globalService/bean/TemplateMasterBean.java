f79b6e9772a520bed790a1246e576cd7cf934edc
Pooja M Nandgaonkar <Pooja.Nandgaonkar@in.ey.com>
2022-12-09 11:44:36 +0530

    added bean class for template master and details

---
+++ global-service/src/main/java/in/cdac/university/globalService/bean/TemplateMasterBean.java
@@ -0,0 +1,56 @@
+package in.cdac.university.globalService.bean;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+import in.cdac.university.globalService.util.annotations.ComboKey;
+import in.cdac.university.globalService.util.annotations.ComboValue;
+import in.cdac.university.globalService.util.annotations.ListColumn;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.Date;
+import java.util.List;
+
+import javax.validation.constraints.NotBlank;
+
+@Getter
+@Setter
+public class TemplateMasterBean {
+    @ListColumn(omit = true)
+    private Long unumTempleId;
+
+    @JsonIgnore
+    private Integer unumIsvalid;
+
+    @JsonIgnore
+    private Date udtEffFrom;
+
+    @JsonIgnore
+    private Date udtEffTo;
+
+    @JsonIgnore
+    private Date udtEntryDate;
+    
+    @JsonIgnore
+    private Long unumEntryUid;
+
+    @JsonIgnore
+    private Integer unumFacultyId;
+    
+    private Integer unumTemplateForYyyy;
+    
+    private Integer unumTemplateForYyyymm;
+    
+    private Integer unumTemplateType;
+    
+    @JsonIgnore
+    private Integer unumUnivId;
+    
+    private String ustrTempDescription;
+    
+    private String ustrTempleCode;
+    
+    private String ustrTempleName;
+    
+    private List<TemplateMasterDtlsBean> templateMasterDtlsBeanList;
+}
---
+++ global-service/src/main/java/in/cdac/university/globalService/bean/TemplateMasterDtlsBean.java
@@ -0,0 +1,61 @@
+package in.cdac.university.globalService.bean;
+
+import java.util.Date;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import in.cdac.university.globalService.util.annotations.ListColumn;
+
+public class TemplateMasterDtlsBean {
+
+	@ListColumn(omit = true)
+	private Long unumTempledtlId;
+
+	@JsonIgnore
+	private Integer unumIsvalid;
+
+	@JsonIgnore
+	private Date udtEffFrom;
+
+	@JsonIgnore
+	private Date udtEffTo;
+
+	@JsonIgnore
+	private Date udtEntryDate;
+
+	@JsonIgnore
+	private Long unumEntryUid;
+
+	@JsonIgnore
+	private Long unumTemplCompItemId;
+
+	@JsonIgnore
+	private Long unumTempleCompId;
+
+	@JsonIgnore
+	private Long unumTempleHeadId;
+
+	@JsonIgnore
+	private Long unumTempleId;
+
+	private Integer unumTempleIsmandy;
+
+	@JsonIgnore
+	private Long unumTempleItemId;
+
+	@JsonIgnore
+	private Long unumTempleSubheadId;
+
+	@JsonIgnore
+	private Integer unumUnivId;
+
+	private String ustrTempledtlDescription;
+
+	private Integer unumDisplayOrder;
+
+	private Integer unumHideComponentTxt;
+
+	private Integer unumHideHeaderTxt;
+
+	private Integer unumHideItemTxt;
+
+	private Integer unumPageColumns;
+}
