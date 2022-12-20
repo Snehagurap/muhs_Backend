2b645332e3d4c131734d6ac73e028edd8b64ac91
sushil.or.k@gmail.com <sushil.or.k@gmail.com>
2022-12-20 12:38:55 +0530

    Configuration changes Admin server added

--- api-gateway/src/main/java/in/cdac/university/apigateway/config/RequestBodyRewrite.java
+++ api-gateway/src/main/java/in/cdac/university/apigateway/config/RequestBodyRewrite.java
@@ -31,16 +31,16 @@ public class RequestBodyRewrite implements RewriteFunction<String, String> {
     }
 
     private void jsonToString(Object body, StringBuilder result) {
-        if (body instanceof Map bodyAsMap) {
+        if (body instanceof Map<?, ?> bodyAsMap) {
             for (Object key: bodyAsMap.keySet()) {
                 Object value = bodyAsMap.get(key);
                 if (value instanceof String valueAsString) {
                     checkValidCharacters(valueAsString);
                     result.append(valueAsString);
-                } else if (value instanceof List valueAsList) {
+                } else if (value instanceof List<?> valueAsList) {
                     for (Object o : valueAsList)
                         jsonToString(o, result);
-                } else if (value instanceof Map valueAsMap) {
+                } else if (value instanceof Map<?,?> valueAsMap) {
                     jsonToString(valueAsMap, result);
                 }
             }
@@ -100,7 +100,7 @@ public class RequestBodyRewrite implements RewriteFunction<String, String> {
 
     private static final List<Pattern> scriptPatterns = new ArrayList<>();
     static {
-
+        scriptPatterns.add(Pattern.compile("<.*>", Pattern.CASE_INSENSITIVE));
         scriptPatterns.add(Pattern.compile("<script>(.*?)</script>", Pattern.CASE_INSENSITIVE));
         scriptPatterns.add(Pattern.compile("</script>", Pattern.CASE_INSENSITIVE));
         scriptPatterns.add(Pattern.compile("<script(.*?)>", Pattern.CASE_INSENSITIVE | Pattern.MULTILINE | Pattern.DOTALL));
@@ -124,18 +124,11 @@ public class RequestBodyRewrite implements RewriteFunction<String, String> {
         scriptPatterns.add(Pattern.compile("onselect(.*?)=", Pattern.CASE_INSENSITIVE | Pattern.MULTILINE | Pattern.DOTALL));
     }
 
-    private static final Pattern tablePattern = Pattern.compile("<.*>", Pattern.CASE_INSENSITIVE);
     private static final Pattern metaCharacterPattern = Pattern.compile("[!@#\\$+()\\[\\]%\\^&*]{3,}");
 
     private void checkValidCharacters(String value) {
 
         Matcher m;
-
-        m = tablePattern.matcher(value);
-        if (m.find()) {
-            throw new InvalidFormValuesException("Invalid characters found in the input: " + value);
-        }
-
         // Filter for 3 or more consecutive meta characters
         m = metaCharacterPattern.matcher(value);
         if (m.find()) {
