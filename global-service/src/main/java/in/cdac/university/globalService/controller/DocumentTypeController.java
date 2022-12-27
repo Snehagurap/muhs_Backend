684b484cd40a8cee07569393b57c320725bd0b51
ARCHITJHA18 <architjha18@gmail.com>
2022-12-27 16:51:01 +0530

    Added Document Services

--- global-service/src/main/java/in/cdac/university/globalService/controller/DocumentTypeController.java
+++ global-service/src/main/java/in/cdac/university/globalService/controller/DocumentTypeController.java
@@ -1,12 +1,15 @@
 package in.cdac.university.globalService.controller;
 
+import in.cdac.university.globalService.bean.DocumentTypeBean;
 import in.cdac.university.globalService.service.DocumentTypeService;
+import in.cdac.university.globalService.util.ListPageUtility;
 import in.cdac.university.globalService.util.ResponseHandler;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+import java.util.Date;
 
 @RestController
 @RequestMapping("/global/documentType")
@@ -19,6 +22,48 @@ public class DocumentTypeController {
     public ResponseEntity<?> getDocumentTypeListForNotification() {
         return ResponseHandler.generateResponse(
             documentTypeService.getDocumentTypeListForNotification()
+        );
+    }
+
+    @GetMapping("listPage/{status}")
+    public ResponseEntity<?> listPageData(@PathVariable("status") Integer status) throws Exception {
+        return ResponseHandler.generateOkResponse(
+                ListPageUtility.generateListPageData(
+                        documentTypeService.listPageData(status)
+                )
+        );
+    }
+
+    @GetMapping("{docId}")
+    public ResponseEntity<?> getDocumentType(@PathVariable("docId") Long docId) {
+        return ResponseHandler.generateResponse(
+                documentTypeService.getDocumentType(docId)
+        );
+    }
+
+    @PostMapping("save")
+    public ResponseEntity<?> saveDocumentTypeDtl(@Valid @RequestBody DocumentTypeBean documentTypeBean) {
+        System.out.print(documentTypeBean.getUnumIsvalid());
+        documentTypeBean.setUdtEntryDate(new Date());
+        documentTypeBean.setUdtEffFrom(new Date());
+        return ResponseHandler.generateResponse(documentTypeService.save(documentTypeBean));
+    }
+
+    @PostMapping("update")
+    public ResponseEntity<?> update(@Valid @RequestBody DocumentTypeBean documentTypeBean) throws Exception {
+        documentTypeBean.setUdtEntryDate(new Date());
+        documentTypeBean.setUdtEffFrom(new Date());
+        return ResponseHandler.generateResponse(
+                documentTypeService.update(documentTypeBean)
+        );
+    }
+
+    @PostMapping("delete")
+    public ResponseEntity<?> delete(@RequestBody Long[] idsToDelete) throws Exception {
+        DocumentTypeBean documentTypeBean = new DocumentTypeBean();
+        documentTypeBean.setUdtEntryDate(new Date());
+        return ResponseHandler.generateResponse(
+                documentTypeService.delete(documentTypeBean, idsToDelete)
         );
     }
 }
