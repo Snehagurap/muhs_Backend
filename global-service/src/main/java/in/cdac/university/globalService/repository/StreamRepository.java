c2d716b8a4b62f052f922be8ccd456dd8b6a602c
sushil.or.k@gmail.com <sushil.or.k@gmail.com>
2023-01-02 11:02:10 +0530

    Changes in Stream Repository

--- global-service/src/main/java/in/cdac/university/globalService/repository/StreamRepository.java
+++ global-service/src/main/java/in/cdac/university/globalService/repository/StreamRepository.java
@@ -1,6 +1,6 @@
 package in.cdac.university.globalService.repository;
 
- 
+
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -19,21 +19,21 @@ public interface StreamRepository extends JpaRepository<GmstStreamMst, GmstStrea
 
     List<GmstStreamMst> findAllByunumIsvalid(int status);
 
-	List<GmstStreamMst> findByUnumIsvalidInAndUstrStreamCodeIgnoreCaseAndUstrStreamFnameIgnoreCase(List<Integer> of,
-			String ustrStreamCode, String ustrStreamFname);
-	
-	
-	 @Query(value = "select (coalesce(max(a.unum_stream_id),0) + 1) from university.gmst_stream_mst a",nativeQuery = true)
-	 Integer getNextId();
-	 
-	 @Modifying(clearAutomatically = true)
-	    @Query("update GmstStreamMst u set u.unumIsvalid = (select coalesce(max(a.unumIsvalid), 2) + 1 " +
-	            "from GmstStreamMst a where a.unumStreamId = u.unumStreamId and a.unumIsvalid > 2) " +
-	            "where u.unumStreamId in (:streamId) and u.unumIsvalid in (1, 2) ")
-		Integer createLog(@Param("streamId") List<Long> streamId);
+    List<GmstStreamMst> findByUnumIsvalidInAndUnumStreamIdIn(List<Integer> isValid, List<Long> streamIds);
 
-	List<GmstStreamMst> findByUnumIsvalidInAndUstrStreamFnameIgnoreCase(List<Integer> of, String ustrStreamFname);
 
-	List<GmstStreamMst> findByUnumIsvalidInAndUstrStreamFnameIgnoreCaseAndUnumStreamIdNot(List<Integer> of,
-			String ustrStreamFname, Long unumStreamId);
+    @Query(value = "select (coalesce(max(a.unum_stream_id),0) + 1) from university.gmst_stream_mst a", nativeQuery = true)
+    Long getNextId();
+
+    @Modifying(clearAutomatically = true)
+    @Query("update GmstStreamMst u set u.unumIsvalid = (select coalesce(max(a.unumIsvalid), 2) + 1 " +
+            "from GmstStreamMst a where a.unumStreamId = u.unumStreamId and a.unumIsvalid > 2), " +
+            "u.udtEffTo = now() " +
+            "where u.unumStreamId in (:streamId) and u.unumIsvalid in (1, 2) ")
+    Integer createLog(@Param("streamId") List<Long> streamId);
+
+    List<GmstStreamMst> findByUnumIsvalidInAndUstrStreamFnameIgnoreCase(List<Integer> of, String ustrStreamFname);
+
+    List<GmstStreamMst> findByUnumIsvalidInAndUstrStreamFnameIgnoreCaseAndUnumStreamIdNot(List<Integer> of,
+                                                                                          String ustrStreamFname, Long unumStreamId);
 }
--- global-service/src/main/java/in/cdac/university/globalService/service/StreamService.java
+++ global-service/src/main/java/in/cdac/university/globalService/service/StreamService.java
@@ -1,62 +1,57 @@
 package in.cdac.university.globalService.service;
 
-import java.util.List;
-import java.util.Optional;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
-import in.cdac.university.globalService.util.Language;
 import in.cdac.university.globalService.bean.StreamBean;
-import in.cdac.university.globalService.bean.SubjectBean;
-import in.cdac.university.globalService.entity.GbltConfigApplicationTracker;
 import in.cdac.university.globalService.entity.GmstStreamMst;
-import in.cdac.university.globalService.entity.GmstSubjectMst;
 import in.cdac.university.globalService.exception.ApplicationException;
 import in.cdac.university.globalService.repository.StreamRepository;
 import in.cdac.university.globalService.util.BeanUtils;
+import in.cdac.university.globalService.util.Language;
 import in.cdac.university.globalService.util.RequestUtility;
 import in.cdac.university.globalService.util.ServiceResponse;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Arrays;
+import java.util.List;
 
 @Service
 public class StreamService {
-	
-	@Autowired
-	private StreamRepository streamRepository;
 
-	@Autowired
+    @Autowired
+    private StreamRepository streamRepository;
+
+    @Autowired
     private Language language;
-	
-	public List<StreamBean> getAllStream(int status) throws Exception {
-     
-		return BeanUtils.copyListProperties(
-				streamRepository.findAllByunumIsvalid(status),
-				StreamBean.class
+
+    public List<StreamBean> getAllStream(int status) {
+
+        return BeanUtils.copyListProperties(
+                streamRepository.findAllByunumIsvalid(status),
+                StreamBean.class
         );
-    } 
-	
-	
-   public List<StreamBean> getStreamCombo() throws Exception {
+    }
+
+    public List<StreamBean> getStreamCombo() throws Exception {
         return BeanUtils.copyListProperties(
                 streamRepository.findByUnumIsvalidAndUnumUnivId(1, RequestUtility.getUniversityId()), StreamBean.class
         );
     }
-    
+
     @Transactional
     public ServiceResponse save(StreamBean streamBean) {
         // Duplicate check
         List<GmstStreamMst> streamMsts = streamRepository.findByUnumIsvalidInAndUstrStreamFnameIgnoreCase(
-                List.of(1, 2),streamBean.getUstrStreamFname());
-        
+                List.of(1, 2), streamBean.getUstrStreamFname());
+
         if (!streamMsts.isEmpty()) {
-        	return ServiceResponse.errorResponse(language.duplicate("Stream", streamBean.getUstrStreamCode()));
+            return ServiceResponse.errorResponse(language.duplicate("Stream", streamBean.getUstrStreamFname()));
         }
         GmstStreamMst gmstStreamMst = BeanUtils.copyProperties(streamBean, GmstStreamMst.class);
-        
+
         // Generate New Id
-        Integer subjectId = streamRepository.getNextId();
-        gmstStreamMst.setUnumStreamId(new Long(subjectId));
-        System.out.print(gmstStreamMst);
+        Long streamId = streamRepository.getNextId();
+        gmstStreamMst.setUnumStreamId(streamId);
         streamRepository.save(gmstStreamMst);
 
         return ServiceResponse.builder()
@@ -64,19 +59,19 @@ public class StreamService {
                 .message(language.saveSuccess("Stream"))
                 .build();
     }
-    
+
     @Transactional
     public ServiceResponse updateStreamDetails(StreamBean streamBean) {
         if (streamBean.getUnumStreamId() == null) {
             return ServiceResponse.errorResponse(language.mandatory("Stream Id"));
         }
-        
+
         // Duplicate check
         List<GmstStreamMst> subjectList = streamRepository.findByUnumIsvalidInAndUstrStreamFnameIgnoreCaseAndUnumStreamIdNot(
-        List.of(1, 2),streamBean.getUstrStreamFname(),streamBean.getUnumStreamId());
-        
+                List.of(1, 2), streamBean.getUstrStreamFname(), streamBean.getUnumStreamId());
+
         if (!subjectList.isEmpty()) {
-            return ServiceResponse.errorResponse(language.duplicate("Stream", streamBean.getUstrStreamCode()));
+            return ServiceResponse.errorResponse(language.duplicate("Stream", streamBean.getUstrStreamFname()));
         }
 
         // Create Log
@@ -84,7 +79,7 @@ public class StreamService {
         if (noOfRecordsAffected == 0) {
             throw new ApplicationException(language.notFoundForId("Stream", streamBean.getUnumStreamId()));
         }
-        
+
         // Save new Data
         GmstStreamMst streamMst = BeanUtils.copyProperties(streamBean, GmstStreamMst.class);
         streamRepository.save(streamMst);
@@ -93,36 +88,50 @@ public class StreamService {
                 .status(1)
                 .message(language.updateSuccess("Stream"))
                 .build();
- }
-    
+    }
+
     @Transactional
-    public ServiceResponse deleteStreamDetails(StreamBean streamBean) {
-        if (streamBean.getUnumStreamId() == null) {
+    public ServiceResponse deleteStreamDetails(StreamBean streamBean, Long[] idsToDelete) {
+        if (idsToDelete == null) {
             return ServiceResponse.errorResponse(language.mandatory("Stream Id"));
         }
-       
-        // Duplicate check
-        List<GmstStreamMst> subjectList = streamRepository.findByUnumIsvalidInAndUstrStreamFnameIgnoreCaseAndUnumStreamIdNot(
-        List.of(1, 2),streamBean.getUstrStreamFname(),streamBean.getUnumStreamId());
 
-        if (!subjectList.isEmpty()) {
-            return ServiceResponse.errorResponse(language.duplicate("Stream", streamBean.getUstrStreamCode()));
+        List<GmstStreamMst> streamMsts = streamRepository.findByUnumIsvalidInAndUnumStreamIdIn(
+                List.of(1, 2), List.of(idsToDelete)
+        );
+
+        if (streamMsts.size() != idsToDelete.length) {
+            return ServiceResponse.errorResponse(language.notFoundForId("Stream Id", Arrays.toString(idsToDelete)));
         }
 
         // Create Log
-        int noOfRecordsAffected = streamRepository.createLog(List.of(streamBean.getUnumStreamId()));
-        if (noOfRecordsAffected == 0) {
-            throw new ApplicationException(language.notFoundForId("Stream", streamBean.getUnumStreamId()));
+        int noOfRecordsAffected = streamRepository.createLog(List.of(idsToDelete));
+        if (noOfRecordsAffected != idsToDelete.length) {
+            throw new ApplicationException(language.notFoundForId("Stream", Arrays.toString(idsToDelete)));
         }
-        
+
         // Save new Data
-        GmstStreamMst streamMst = BeanUtils.copyProperties(streamBean, GmstStreamMst.class);
-        streamMst.setUnumIsvalid(0);
-        streamRepository.save(streamMst);
-        
+        streamMsts.forEach(stream -> {
+            stream.setUnumIsvalid(0);
+            stream.setUdtEntryDate(streamBean.getUdtEntryDate());
+        });
+        streamRepository.saveAll(streamMsts);
+
         return ServiceResponse.builder()
                 .status(1)
                 .message(language.updateSuccess("Stream"))
                 .build();
- }
+    }
+
+    public StreamBean getStreamById(Long streamId) {
+        List<GmstStreamMst> streamById = streamRepository.findByUnumIsvalidInAndUnumStreamIdIn(
+                List.of(1, 2), List.of(streamId)
+        );
+        if (streamById.isEmpty())
+            throw new ApplicationException("Stream not found");
+        return BeanUtils.copyProperties(
+                streamById.get(0),
+                StreamBean.class
+        );
+    }
 }
