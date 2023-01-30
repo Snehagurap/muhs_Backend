05204ec1db621db6f4a9a2e94e63a85829fb3d18
sushil.or.k@gmail.com <sushil.or.k@gmail.com>
2023-01-30 23:21:13 +0530

    Applicant auto approval

--- global-service/src/main/java/in/cdac/university/globalService/controller/MasterTemplateController.java
+++ global-service/src/main/java/in/cdac/university/globalService/controller/MasterTemplateController.java
@@ -13,8 +13,6 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
-
-import java.util.Date;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
